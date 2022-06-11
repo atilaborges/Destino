194b7fecf0567e31074d657ec7687ddc8df03510
Atila Borges <claudioatila@gmail.com>
2022-06-10 22:34:39 -0300

    upando projeto

---
+++ app.js
@@ -0,0 +1,50 @@
+const inputPergunta = document.querySelector("#inputPergunta")
+const elementoResposta = document.querySelector("#resposta")
+const buttonPerguntar = document.querySelector("#buttonPerguntar")
+const respostas = [
+  'Não!',
+  'Provavelmente..',
+  'Sim',
+  'Com certeza!',
+  'Olha, acho que não..',
+  'Minhas fontes, dizem que não!',
+  'Minhas fontes, dizem que sim!',
+  'Sem sombra de dúvidas!',
+  'você pode contar com isso!',
+  'Minha resposta é não!',
+  'Quem sabe..',
+  'Preciso checar de novo..',
+  'Humm, tenta de novo..',
+  'Se concentra mais um cadinho..',
+  'Ahhh eu tô tão cansado',
+  'Então, a resposta está dentro de você!',
+  'Só um minuto, deu boi na linha...',
+  'Pode celebrar! é siiiim'
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+
+  // pegar pergunta
+  if(inputPergunta.value == ""){
+  alert('Digite sua pergunta!')
+  return
+  }
+
+  buttonPerguntar.setAttribute('disabled', true)
+
+  const pergunta = '<div>' + inputPergunta.value + '</div>'
+
+  //gerar número aleatório
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+  
+  elementoResposta.style.opacity = 1;
+  // sumir resposta depois de 3 seg
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute('disabled')
+  }, 3000)
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# HTML
+- HyperText Markup Language
+
+- Hiper Texto?
+- Marcação
+  - tags
+  - atributos
+- Linguagem
+  - maneira de escrever
+
+# CSS
+
+- Apresentação visual para o cliente
+- Estilos para o HTML
+- Cascading Style Sheets
+  - Folha de Estilo em Cascata
+
+# Declaração
+- Seletor
+- Propriedade e Valor
+
+# Conceitos
+- Cascata
+- Especificidade
+- Box Model
+- Display block vs inline
+
+
+
+### imagem bola de cristal
+https://upload.wikimedia.org/wikipedia/commons/d/da/743-crystal-ball-1.svg
